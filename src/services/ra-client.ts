abe95a33b741b7d93fda5dce8d2f56ff3d1939db
Miguel Mano <hi@mano.sh>
2026-07-20 02:53:51 +0100

    fix atari lynx file extension: .lyx -> .lnx

--- src/services/ra-client.ts
+++ src/services/ra-client.ts
@@ -3,6 +3,16 @@ import { getAuthorization, isVariantSupported, searchTitle } from "../api/ra";
 import { getRASystemId } from "../constants/systems";
 import type { MatchGameMessageRequest } from "../types/messages";
 
+type TitleTransform = (title: string) => string;
+
+const titleTransforms: Record<number, TitleTransform> = {
+	// atari lynx
+	13: (str) => {
+		// replace extension: .lyx (vimm's) -> .lnx (RA)
+		return str.replace(/\.lyx$/, ".lnx");
+	},
+};
+
 // @ts-expect-error
 browser.runtime.onMessage.addListener(async (m) => {
 	const message = m as MatchGameMessageRequest;
@@ -20,6 +30,12 @@ browser.runtime.onMessage.addListener(async (m) => {
 		return { type: "inactiveSystem" };
 	}
 
+	let normalizedTitle = message.gameVariant;
+
+	if (system.id in titleTransforms) {
+		normalizedTitle = titleTransforms[system.id](normalizedTitle);
+	}
+
 	const gameId = await searchTitle(message.gameTitle, system.id);
 	if (!gameId) {
 		return { type: "notFound" };
@@ -28,7 +44,7 @@ browser.runtime.onMessage.addListener(async (m) => {
 	const isSupported = await isVariantSupported(
 		authorization,
 		gameId,
-		message.gameVariant,
+		normalizedTitle,
 	);
 	return { type: "success", gameId, isSupported };
 });
